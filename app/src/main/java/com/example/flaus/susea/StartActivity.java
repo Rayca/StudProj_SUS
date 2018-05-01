d0e6b412c92f2c37babfdbf5770ae71f6e77ae44
Wolfskid <ruuqo@online.de>
2018-05-01 11:55:57 +0200

    Test Activity wird erzeugt ist aber noch Empty

--- app/src/main/java/com/example/flaus/susea/StartActivity.java
+++ app/src/main/java/com/example/flaus/susea/StartActivity.java
@@ -1,7 +1,9 @@
 package com.example.flaus.susea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,7 +12,12 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        //neuer Kommentar
-        //dummer zweiter Kommentar
+
+    }
+
+
+    public void startt(View view){
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivity(intent);
     }
 }
