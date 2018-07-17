b92be042642400ac09d7f7b8bb6d6001e9759adf
thomasjager <thomas.jaeger94@icloud.com>
2018-07-17 17:02:34 +0200

    typactivity steht soweit

---
+++ app/src/main/java/com/example/flaus/susea/AuswertungsActivities/AuswertungTestActivity.java
@@ -0,0 +1,119 @@
+package com.example.flaus.susea.AuswertungsActivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.flaus.susea.AuswertungsActivities.AuswertungStudieActivity;
+import com.example.flaus.susea.Datenbank;
+import com.example.flaus.susea.ListViewActivities.ListViewStudienActivity;
+import com.example.flaus.susea.R;
+import com.example.flaus.susea.StartActivity;
+import com.example.flaus.susea.Statistik;
+
+public class AuswertungTestActivity extends AppCompatActivity {
+
+    TextView textViewScore, textViewGeschlecht, textViewAlter, textViewAntworten;
+    Button btnZurStudie, btnZurStartseite;
+    long testId;
+    long studienId = -1;
+    int[] antworten = new int[10];
+    boolean studie;
+    String studienName;
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auswertung_test);
+
+        Datenbank mDatenbankManager = new Datenbank(this);
+
+        // Toolbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Auswertung Test");
+
+        // View-Binding
+        textViewScore = (TextView) findViewById(R.id.textViewScore);
+        textViewGeschlecht = (TextView) findViewById(R.id.textViewGeschlecht);
+        textViewAlter = (TextView)findViewById(R.id.textViewAlter);
+        textViewAntworten = (TextView) findViewById(R.id.textViewAntworten);
+        btnZurStudie = (Button) findViewById(R.id.btnStudie);
+        btnZurStartseite = (Button) findViewById(R.id.btnZurStartseite);
+
+
+        // Intent empfangen
+        Intent intent = getIntent();
+        testId = intent.getLongExtra("testId",-1);
+        studie = intent.getBooleanExtra("studie", false);
+        antworten = intent.getIntArrayExtra("antworten");
+        studienName = intent.getStringExtra("studienName");
+        studienId = intent.getLongExtra("studienId",-1);
+
+
+        Log.d("studie", "AuswertungTest =" + studie);
+
+
+
+        // Button Text ändert sich je nach dem, ob es einzelner Test war oder ob man von einer Studie kommt
+
+        if(studie){
+            btnZurStudie.setText("Zurück zur Studien Activity");
+        } else {
+            btnZurStudie.setText("Test einer Studie zuordnen");
+        }
+
+
+        // Funktion für btnZurStudie
+
+        btnZurStudie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(studie){
+                    Intent intent1 = new Intent(getApplicationContext(),AuswertungStudieActivity.class);
+                    intent1.putExtra("testId", testId);
+                    intent1.putExtra("studie",studie);
+                    intent1.putExtra("studienName", studienName);
+                    intent1.putExtra("studienId", studienId);
+                    startActivity(intent1);
+                } else {
+                    Intent intent2 = new Intent(getApplicationContext(),ListViewStudienActivity.class);
+                    intent2.putExtra("testId", testId);
+                    intent2.putExtra("studie",studie);
+                    startActivity(intent2);
+                }
+            }
+        });
+
+
+
+
+        // Funktion für btnStartseite
+        btnZurStartseite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        // TextViews füllen
+        textViewScore.setText(" " + Statistik.berechneScore(antworten));
+
+
+        // TODO : geschlecht und alter angeben und antworten einzelner fragen
+        // TODO : Button zurück zur StudienActivity mit funktion versetzen. intent muss alles mitgeben
+
+
+
+    }
+}
--- app/src/main/java/com/example/flaus/susea/StartActivity.java
+++ app/src/main/java/com/example/flaus/susea/StartActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.flaus.susea.ListViewActivities.ListViewStudienActivity;
@@ -22,6 +23,8 @@ public class StartActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("System Usability Scale");
+
+
 
     }
 
