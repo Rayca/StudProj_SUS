1c716d152a94935f82dfe2c2d25ef96898ec0f04
Wolfskid <ruuqo@online.de>
2018-07-19 18:08:50 +0200

    funktionen für Standardabweichung, Mittelwert, usablity und learnability eingefügt. Median noch nicht vorhanden.

--- app/src/main/java/com/example/flaus/susea/Statistik.java
+++ app/src/main/java/com/example/flaus/susea/Statistik.java
@@ -6,24 +6,52 @@ import android.util.Log;
 /* Stellt die mathematischen Funktionen zu Verfügung um Kennzahlen für Studien zu berechnen */
 public class Statistik {
 
-    public static int berechneStandardabweichung(int[] antworten){
-
-        int standardAbweichung = 0;
-
-
+    public static double berechneStandardabweichung(int[] antworten){
+        double varianz = 0;
+        double standardAbweichung = 0;
+        double mittelWert = mittelWert(antworten);
+        for(int i = 0;i<antworten.length;i++){
+            varianz+=antworten[i] - mittelWert;
+        }
+        varianz = varianz/antworten.length;
+        standardAbweichung = Math.sqrt(varianz);
 
         return standardAbweichung;
     }
 
-    public static double berechneCronbachAlpha(int[] antworten){
-
-        double cronbachAlpha = 0;
-
-
-
-        return cronbachAlpha;
+    public static double mittelWert(int[] antworten){
+        double result;
+        int wert = 0;
+        for(int i = 0;i<antworten.length;i++){
+            wert+=antworten[i];
+        }
+        result = wert/antworten.length;
+        return result;
     }
 
+    public static int median(int[]antworten){
+        int median=0;
+        return median;
+    }
+
+    public static int usability(int[]antworten){
+        int usability=0;
+        for(int i = 0; i<antworten.length;i++){
+
+            usability += antworten[i] - 1;
+
+        }
+        usability-=(antworten[3]+antworten[9]);
+        usability*=3.125;
+
+        return usability;
+    }
+
+    public static int learnability(int[]antworten){
+        double learnability;
+        learnability=(12.5)*(antworten[3]+antworten[9]);
+        return (int) learnability;
+    }
 
     public static int berechneScore(int[] antworten){
         double score = 0;
